c2c4f85e9222db5fccb50f0b724d1bf9d478d557
PhamDuy-k3 <phamdoduy123@gmail.com>
2024-05-21 08:16:04 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ import express from "express";
 import { router } from "./Router/index.js";
 import { connect } from "./config/database/mongo.js";
 import cors from "cors";
+import { createServer } from "http"; // Import createServer from http module
+import { Server } from "socket.io"; // Import Server from socket.io
+
 const app = express();
+
+const server = createServer(app); // Create HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+  },
+});
+
 app.use(express.json());
 app.use(cors());
 
@@ -12,7 +23,21 @@ router(app);
 
 app.use(express.static("storage/user"));
 
-//khởi tạo server
-app.listen(5050, () => {
-  console.log("sever listening on port 3000");
+io.on("connection", (socket) => {
+  socket.on("message", (msg) => {
+    // lấy dữ liệu từ client khi user send
+    const messageWithTimestamp = {
+      text: msg,
+      timestamp: new Date().toISOString(),
+    };
+    socket.broadcast.emit("message", messageWithTimestamp); // gửi thông điệp này đến tất cả các client khác ngoại trừ sender
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(5050, () => {
+  console.log("server listening on port 5050");
 });
